8261e4ba8395c3507313b2ab0760727545a20c67
lud0m4n <fruktin03@gmail.com>
2024-04-23 22:18:01 +0300

    init

---
+++ check_new.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify
+import requests  # Импортируем библиотеку requests
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModel
+import pickle
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import re
+import string
+
+app = Flask(__name__)
+
+tokenizer = AutoTokenizer.from_pretrained("cointegrated/LaBSE-en-ru")
+model = AutoModel.from_pretrained("cointegrated/LaBSE-en-ru")
+with open('kmeans_model.pkl', 'rb') as file:
+    kmeans = pickle.load(file)
+
+def preprocess_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = re.sub(r'\b(привет|добрый день|доброе утро|добрый вечер|здравствуйте)\b', '', text)
+    tokens = word_tokenize(text, language='russian')
+    tokens = [token for token in tokens if token not in stopwords.words('russian')]
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    return " ".join(tokens)
+
+def get_message_embedding(message):
+    message = preprocess_text(message)
+    encoded_input = tokenizer(message, padding=True, truncation=True, max_length=64, return_tensors='pt')
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+    return np.asarray(model_output.pooler_output[0].numpy())
+
+@app.route('/cluster', methods=['POST'])
+def classify_message():
+    data = request.get_json()
+    new_message = data['message']
+    embedding = get_message_embedding(new_message)
+    kmeans.cluster_centers_ = kmeans.cluster_centers_.astype(float)
+    cluster_label = kmeans.predict([embedding])[0]
+    cluster_center = kmeans.cluster_centers_[cluster_label]
+    distance = np.linalg.norm(embedding - cluster_center)
+    threshold = 10
+    if distance < threshold:
+        response = {
+            "message": f"Message is classified into cluster {cluster_label} with distance {distance}."
+        }
+    else:
+        response = {
+            "message": "Message does not fit into any existing cluster."
+        }
+
+    # Отправляем результат на другой сервер
+    try:
+        send_url = 'http://localhost:8081/cluster'
+        requests.post(send_url, json=response)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # Возвращаем ошибку, если что-то пошло не так
+
+    return jsonify(response)  # Возвращаем результат также локальному клиенту
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ main.py
@@ -0,0 +1,134 @@
+import json
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import string
+from sklearn.manifold import TSNE
+import numpy as np
+import torch
+import os
+from transformers import AutoTokenizer, AutoModel
+import matplotlib.pyplot as plt
+import pickle
+import re
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+# Загрузка данных
+with open("dataset_hack.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# Создание DataFrame
+df = pd.DataFrame(data)
+
+# Выборка колонки с сообщениями
+messages = df['message'].tolist()
+def preprocess_text(text):
+    text = text.lower()  # Приведение к нижнему регистру
+    text = re.sub(r'\b(привет|добрый день|доброе утро|добрый вечер|здравствуйте)\b', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))  # Удаление знаков пунктуации
+    tokens = word_tokenize(text, language='russian')  # Токенизация
+    tokens = [token for token in tokens if token not in stopwords.words('russian')]  # Удаление стоп-слов
+    lemmatizer = WordNetLemmatizer()  # Инициализация лемматизатора
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]  # Лемматизация
+    return " ".join(tokens)
+
+# Применение функции предобработки к каждому сообщению в списке
+messages = [preprocess_text(message) for message in messages]
+tokenizer = AutoTokenizer.from_pretrained("cointegrated/LaBSE-en-ru")
+model = AutoModel.from_pretrained("cointegrated/LaBSE-en-ru")
+
+def embedding(messages):
+    filename = 'embeddings.npy'
+    # Проверяем, существует ли файл с векторами
+    if os.path.exists(filename):
+        print("Loading embeddings from file...")
+        embeddings = np.load(filename)
+    else:
+        print("Calculating embeddings...")
+        embeddings_list = []
+        for message in messages:
+            encoded_input = tokenizer(message, padding=True, truncation=True, max_length=64, return_tensors='pt')
+            with torch.no_grad():
+                model_output = model(**encoded_input)
+            embeddings_list.append(model_output.pooler_output[0].numpy())
+        embeddings = np.asarray(embeddings_list)
+        np.save(filename, embeddings)  # Сохраняем векторы в файл
+
+    return embeddings
+
+
+from sklearn.cluster import KMeans
+
+embeddings = embedding(messages)
+# Определение количества кластеров (используя функцию determine_k)
+k_min = 2
+k_max = 25
+clusters = range(k_min, k_max + 1)
+inertia = [KMeans(n_clusters=k, random_state=42).fit(embeddings).inertia_ for k in clusters]
+plt.plot(clusters, inertia, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.title('Elbow Method For Optimal k')
+plt.show()
+
+
+elbow_data = pd.DataFrame({
+    'Clusters': clusters,
+    'Inertia': inertia
+})
+elbow_filename = 'elbow_data.csv'
+elbow_data.to_csv(elbow_filename, index=False)
+print(f"Elbow data saved to {elbow_filename}.")
+#
+k = 13
+#
+
+# После визуализации выбираем k в зависимости от графика
+
+kmeans = KMeans(n_clusters=k, random_state=42)
+df['cluster'] = kmeans.fit_predict(embeddings)
+
+
+cluster_counts = df['cluster'].value_counts()
+plt.bar(cluster_counts.index, cluster_counts.values)
+plt.xlabel('Cluster')
+plt.ylabel('Frequency')
+plt.title('Frequency of Messages by Cluster')
+plt.show()
+
+
+# Сохранение модели KMeans
+kmeans_model_filename = 'kmeans_model.pkl'
+with open(kmeans_model_filename, 'wb') as file:
+    pickle.dump(kmeans, file)
+
+print(f"KMeans model saved to {kmeans_model_filename}.")
+
+
+tsne = TSNE(n_components=2, random_state=42)
+embeddings_2d = tsne.fit_transform(embeddings)
+tsne_data = pd.DataFrame(embeddings_2d, columns=['t-SNE Feature 1', 't-SNE Feature 2'])
+tsne_data['cluster'] = df['cluster']
+tsne_filename = 'tsne_data.csv'
+tsne_data.to_csv(tsne_filename, index=False)
+print(f"t-SNE data saved to {tsne_filename}.")
+# Визуализация
+plt.figure(figsize=(8, 6))
+plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=df['cluster'], cmap='viridis', alpha=0.5)
+plt.colorbar()
+plt.title('t-SNE Visualization of Message Embeddings')
+plt.xlabel('t-SNE Feature 1')
+plt.ylabel('t-SNE Feature 2')
+plt.show()
+# Для вывода примеров сообщений из каждого кластера
+for cluster in sorted(df['cluster'].unique()):
+    print(f"Cluster {cluster}:")
+    print(df[df['cluster'] == cluster]['message'].head())
+csv_filename = 'clustered_messages.csv'
+df.to_csv(csv_filename, index=False)
+print(f"Data saved to {csv_filename}.")
+
